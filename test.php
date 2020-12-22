70bdab7079542493eb9f514bdf4e4d7e534dfd7c
Maksym Sukhomlynov <lbstalker512@gmail.com>
2020-12-22 14:27:18 +0200

    new way fix

--- test.php
+++ test.php
@@ -1,27 +1,28 @@
+
 <?php
 
 function getMinMax($textFile, $word1, $word2)
 {
-    $words = explode(' ', preg_replace('([\x0a,;.?!])', '', file_get_contents($textFile)));
-    $pos1 = array_search($word1, $words);
-    $pos2 = array_search($word2, $words);
+    $words = preg_split('([\x0a\s])', file_get_contents($textFile));
     $min = PHP_INT_MAX;
     $max = 0;
+    $size = sizeof($words);
+
 
     foreach ($words as $key => $word) {
         if ($word == $word1) {
-            $pos1 = $key;
-            $gap = abs($pos1 - $pos2);
-            if ($pos2 >= 0) {
-                if ($min > $gap) $min = $gap;
-                elseif ($max < $gap) $max = $gap;
+            for($i = $key; $i < $size; $i++) {
+                if($words[$i] == $word2 && ($i - $key) < $min) { $min = ($i - $key); break;}
+            }
+            for($i = $size - 1; $i > $key; $i--) {
+                if($words[$i] == $word2 && ($i - $key) > $max) { $max = ($i - $key); break;}
             }
         } elseif ($word == $word2) {
-            $pos2 = $key;
-            $gap = abs($pos2 - $pos1);
-            if ($pos1 >= 0) {
-                if ($min > $gap) $min = $gap;
-                elseif ($max < $gap) $max = $gap;
+            for($i = $key; $i < $size; $i++) {
+                if($words[$i] == $word1 && ($i - $key) < $min) { $min = ($i - $key); break;}
+            }
+            for($i = $size - 1; $i > $key; $i--) {
+                if($words[$i] == $word1 && ($i - $key) > $max) { $max = ($i - $key); break;}
             }
         }
     }
